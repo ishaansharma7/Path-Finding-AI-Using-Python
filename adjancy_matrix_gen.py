0ccbd3cd3dc0b0d9cc5829defb366e362d9970bd
ishaansharma7 <56833627+ishaansharma7@users.noreply.github.com>
2020-04-15 14:26:27 +0530

    Update adjancy_matrix_gen.py

--- adjancy_matrix_gen.py
+++ adjancy_matrix_gen.py
@@ -27,43 +27,44 @@
 #         else:
 #             return 0
 
-def relation(l, r):
+
+def relation(i, j):
     vertical = 20
     vertical_rem = 19
-    diagonal1 = 19
-    diagonal2 = 21
-    if l == r:
+    if i == j:                                                          # check whether the vertices are same
         return 0
-    if l%vertical != 0 and l%vertical!=vertical_rem:  # check not leftest or rightest column
-        if l+1==r or l-1==r or l+vertical==r or l-vertical==r:
+    if i % vertical != 0 and i % vertical != vertical_rem:              # check not first or last column
+        if i+1 == j or i-1 == j or i+vertical == j or i-vertical == j:
             return 1
 
         else:
             return 0
-    elif l%vertical==0:
-        if l+1==r or l+vertical==r or l-vertical==r:
+    elif i % vertical == 0:                                             # if first column
+        if i+1 == j or i+vertical == j or i-vertical == j:
             return 1
 
         else:
             return 0
-    elif l%vertical==vertical_rem:
-        if l-1==r or l+vertical==r or l-vertical==r:
+    elif i % vertical == vertical_rem:                                  # if last column
+        if i-1 == j or i+vertical == j or i-vertical == j:
             return 1
 
         else:
             return 0
+
 
 row_col_size = 20
 size = row_col_size*row_col_size
-adjancey_mat = [[0 for i in range(size)] for j in range(size)]
+adjacencey_mat = [[0 for i in range(size)] for j in range(size)]
 
 for i in range(0, size):
     for j in range(0, size):
-        result = relation(i,j)
-        adjancey_mat[i][j] = result
+        result = relation(i, j)
+        adjacencey_mat[i][j] = result
+
 
 def return_matrix():
-    return adjancey_mat, size
+    return adjacencey_mat, size
 
 
 
